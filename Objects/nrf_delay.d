872a9e65d6c81436b24f2a2a950361ae99da2a39
Vladimir_135 <bvladimirv135@gmail.com>
2020-02-06 12:16:06 +0300

    Начальный пример blinky с вырезаyными log and board

---
+++ Objects/nrf_delay.d
@@ -0,0 +1,17 @@
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Drivers\4.0.0-2.alpha\delay\nrf_delay.c
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\compiler_abstraction.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\nrf.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\nrf52.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\system_nrf52.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\system_nrf.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\nrf52_bitfields.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\nrf51_to_nrf52.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.29.0\Device\Include\nrf52_name_change.h
+.\objects\nrf_delay.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_Drivers\4.0.0-2.alpha\delay\nrf_delay.h
